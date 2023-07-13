fc77737ba0ab22f81b45fbcd563acbd36d3b0f45
akshita2903 <akshita29320@gmail.com>
2023-07-13 12:10:07 +0530

    loop

---
+++ loops/for.py
@@ -0,0 +1,7 @@
+for i in range(5):  #i=0 to i<=4
+    print(i *i ,' ')
+
+print("Set")    
+s={1,5,20,45}
+for i in s:
+    print(i)
---
+++ loops/while.py
@@ -0,0 +1,5 @@
+while(True):
+    num=(int)(input("Enter"))
+    if num  == 0:
+        break
+    print(num)   
